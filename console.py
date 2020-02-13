9c0682281e5954165d19159a429517e4b7508d17
daod2003 <1253@holbertonschool.com>
2020-02-13 22:20:24 +0000

    modified consoleand basemodel

--- console.py
+++ console.py
@@ -4,13 +4,27 @@ Create the console
 """
 
 
-import cmd, sys
+import cmd
 
 
 class HBNBCommand(cmd.Cmd):
     """ Console """
     prompt = '(hbnb) '
     file = None
+
+    def do_EOF(self, line):
+        """Function that do EOF"""
+        return True
+
+    def do_quit(self, line):
+        """Function that close the program"""
+        return True
+
+    def emptyline(self):
+        """Called when an empty line is entered in response to the prompt"""
+        if self.lastcmd:
+            self.lastcmd = ""
+            return self.onecmd('\n')
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
--- models/base_model.py
+++ models/base_model.py
@@ -7,9 +7,26 @@ Here you can find the base model function
 import uuid
 from datetime import datetime
 
-time = %Y-%m-%dT%H:%M:%S.%f
+time = "%Y-%m-%dT%H:%M:%S.%f"
 
 class BaseModel:
-    """ Here start the BaseModel function """
-    def __init__(self, uid=None):
-        
+    """Here start the BaseModel function"""
+    def __init__(self):
+        """ here init the function """
+        self.id = str(uuid.uuid4())
+        self.create_at = datetime.now()
+        self.update_at = self.create_at
+
+    def __str__(self):
+        """ Function that prints class name, id and __dic """
+        pass
+
+    def save(self):
+        """ Function that updates the public instance attribute
+updated_at with the current datetime """
+        pass
+
+    def to_dict(self):
+        """ returns a dictionary containing all keys/values of dict
+ of the instance """
+        pass
