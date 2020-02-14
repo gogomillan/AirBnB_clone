2fe2c95a56431a4c18fb2c5738745ae194716589
daod2003 <1253@holbertonschool.com>
2020-02-14 15:46:28 +0000

    edit Readme

--- console.py
+++ console.py
@@ -20,6 +20,29 @@ class HBNBCommand(cmd.Cmd):
         """Function that close the program"""
         return True
 
+    def do_create(self, line):
+        """Creates a new instace of basemodel"""
+        print("this is a funny method")
+
+    def do_show(self, line):
+        """Prints ths string representation of an
+instace based on the class and id"""
+        print("this is a funny method")
+
+    def do_destroy(self, line):
+        """Deletes an instance based on the class name and id"""
+        print("this is a funny method")
+
+    def do_all(self, line):
+        """Prints all string representation of all instances based or
+not on the class name"""
+        print("this is a funny method")
+
+    def do_update(self, line):
+        """Updates an instance based on the class name and id by adding
+or updating attributes"""
+        print("this is a funny method")
+
     def emptyline(self):
         """Called when an empty line is entered in response to the prompt"""
         if self.lastcmd:
