24b1b43adb3c6a1ea5292108a699ec6f93a4aa45
daod2003 <1253@holbertonschool.com>
2020-02-15 16:26:58 +0000

    finish the def to_dict

--- models/base_model.py
+++ models/base_model.py
@@ -7,7 +7,8 @@ import models
 import uuid
 from datetime import datetime
 
-time = "%Y-%m-%dT%H:%M:%S.%f"
+#time = "%Y-%m-%dT%H:%M:%S.%f"
+
 
 class BaseModel:
     """Here start the BaseModel function"""
@@ -18,13 +19,13 @@ class BaseModel:
         self.update_at = self.create_at
 
     def __str__(self):
-        """ Function that prints class name, id and __dic """
+        """Function that prints class name, id and __dic"""
         print("[{:s}] ({:s}) {}".format(self.__class__.__name__, self.id,
                                         self.__dict__))
 
     def save(self):
-        """ Function that updates the public instance attribute
-updated_at with the current datetime """
+        """Function that updates the public instance attribute
+updated_at with the current datetime"""
         self.updated_at = datetime.now()
 
     def to_dict(self):
@@ -32,4 +33,8 @@ updated_at with the current datetime """
  of the instance """
         new_dict = self.__dict__.copy()
         if "created_at" in __dict__:
-            
+            new_dict["created_at"] = new_dict["created_at"].isoformat()
+        if "updated_at" in self.__dict__:
+            new_dict["updated_at"] = new_dict["updated_at"].isoformat()
+        new_dict["__class__"] = self.__class__.__name__
+        return new_dict
