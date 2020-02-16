78d9e4662918d95c2c6bdaaf21a15c03c98822bf
daod2003 <1253@holbertonschool.com>
2020-02-16 16:35:12 +0000

    add args and kwargs to basemodel and fixed bugs of variable updated and created

--- models/base_model.py
+++ models/base_model.py
@@ -7,21 +7,30 @@ import models
 import uuid
 from datetime import datetime
 
-#time = "%Y-%m-%dT%H:%M:%S.%f"
+time = "%Y-%m-%dT%H:%M:%S.%f"
 
 
 class BaseModel:
     """Here start the BaseModel function"""
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """ here init the function """
-        self.id = str(uuid.uuid4())
-        self.create_at = datetime.now()
-        self.update_at = self.create_at
+        if kwargs:
+            for key, value in kwargs.items():
+                if key != "__class__":
+                    setattr(self, key, value)
+            if hasattr(self, "created_at") and type(self.created_at) is str:
+                self.created_at = datetime.strptime(kwargs["created_at"], time)
+            if hasattr(self, "updated_at") and type(self.updated_at) is str:
+                self.updated_at = datetime.strptime(kwargs["updated_at"], time)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = self.created_at
 
     def __str__(self):
         """Function that prints class name, id and __dic"""
-        print("[{:s}] ({:s}) {}".format(self.__class__.__name__, self.id,
-                                        self.__dict__))
+        return "[{:s}] ({:s}) {}".format(self.__class__.__name__, self.id,
+                                        self.__dict__)
 
     def save(self):
         """Function that updates the public instance attribute
@@ -32,9 +41,9 @@ updated_at with the current datetime"""
         """ returns a dictionary containing all keys/values of dict
  of the instance """
         new_dict = self.__dict__.copy()
-        if "created_at" in __dict__:
+        if "created_at" in new_dict:
             new_dict["created_at"] = new_dict["created_at"].isoformat()
-        if "updated_at" in self.__dict__:
+        if "updated_at" in new_dict:
             new_dict["updated_at"] = new_dict["updated_at"].isoformat()
         new_dict["__class__"] = self.__class__.__name__
         return new_dict
