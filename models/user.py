2082017148d7386fea9d867d64dab84a1da7a0e8
daod2003 <1253@holbertonschool.com>
2020-02-19 15:03:27 +0000

    fixed code from class user

--- models/user.py
+++ models/user.py
@@ -9,6 +9,11 @@ from models.base_model import BaseModel
 
 class User(BaseModel):
     """Here start the class user"""
+    email = ""
+    password = ""
+    first_name = ""
+    last_name = ""
+
     def __init__(self, *args, **kwargs):
         """Initializes user"""
         super().__init__.(*args, **kwargs)
