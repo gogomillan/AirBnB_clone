ad2031099955f22c84d9c021d1cd2e6d2084787c
daod2003 <1253@holbertonschool.com>
2020-02-19 17:14:32 +0000

    made the import to files filestorage and consolo

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -6,6 +6,12 @@ Here you can find the FileStorage Class
 import json
 from models.base_model import BaseModel
 from models.user import User
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+
 
 classes = {"BaseModel": BaseModel, "User": User, "Amenity": Amenity,
            "City": City, "Place": Place, "Review": Review, "State": State}
